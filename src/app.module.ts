5c2136b755fab6d3f73bc182604e65a1207a8ca5
aiirononeko <aiirononeko@gmail.com>
2023-03-28 11:33:13 +0900

    create graphql server sample

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { PostsResolver } from './posts/posts.resolver';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  providers: [PrismaService, PostsResolver],
+})
+export class AppModule {}
